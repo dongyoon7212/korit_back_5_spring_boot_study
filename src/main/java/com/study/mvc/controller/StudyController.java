ae1a0183f8f6dc7057d9be26e8c0369cc355ad88
dongyoon7212 <dongyoon7212@naver.com>
2024-03-04 11:14:55 +0900

    feat: test model

--- src/main/java/com/study/mvc/controller/StudyController.java
+++ src/main/java/com/study/mvc/controller/StudyController.java
@@ -10,12 +10,13 @@ public class StudyController {
 
     @GetMapping("/hello")
     public ModelAndView helloPage(Model model) {
-
+        model.addAttribute("name", "이동윤");
         return new ModelAndView("hello");
     }
 
     @GetMapping("/test")
     public String testPage(Model model) {
+        model.addAttribute("age", 26);
         return "test";
     }
 }
