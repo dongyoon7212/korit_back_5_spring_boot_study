c67da97f96f204e1ce8d6b3237990c872bedb8e1
dongyoon7212 <dongyoon7212@naver.com>
2024-03-04 16:28:13 +0900

    feat: StudentList findStudent

--- src/main/java/com/study/mvc/controller/StudentController.java
+++ src/main/java/com/study/mvc/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.study.mvc.controller;
 
 import com.study.mvc.dto.StudentReqDto;
-import com.study.mvc.dto.StudentRespDto;
+import com.study.mvc.entity.Student;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentController {
@@ -15,5 +19,25 @@ public class StudentController {
         System.out.println(studentReqDto);
 
         return ResponseEntity.badRequest().header("test", "header_test").body(studentReqDto.toRespDto());
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<?> getStudent(@PathVariable int studentId) { //주소 안 값, 아예 생략은 불가
+        List<Student> studentList = List.of(
+                new Student(1, "이동윤"),
+                new Student(2, "삼동윤"),
+                new Student(3, "사동윤"),
+                new Student(4, "오동윤")
+        );
+        Student findStudent = null;
+        for(Student student : studentList){
+            if(student.getStudentId() == studentId){
+                findStudent = student;
+            }
+        }
+        if(findStudent == null) {
+            return ResponseEntity.badRequest().body(Map.of("errorMessage", "존재하지 않는 ID 입니다."));
+        }
+        return ResponseEntity.ok().body(findStudent);
     }
 }
