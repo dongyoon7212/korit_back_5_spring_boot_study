61fc088d47eaad830f7f4c4c91a422ed39eaa8b3
dongyoon7212 <dongyoon7212@naver.com>
2024-03-04 14:17:16 +0900

    feat: ResponseBody

--- src/main/java/com/study/mvc/controller/StudyController.java
+++ src/main/java/com/study/mvc/controller/StudyController.java
@@ -1,5 +1,6 @@
 package com.study.mvc.controller;
 
+import com.study.mvc.model.HelloModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +13,19 @@ import java.util.Map;
 @Controller
 public class StudyController {
 
+    //등록된 주소가 없으면 404 오류가 뜬다
+
     //ModelAndView 는 맵으로 데이터 전달 가능
     //MVC
     @GetMapping("/hello")
-    public ModelAndView helloPage() {
-        Map<String, Object> model = new HashMap<>();
-        model.put("name1", "이동윤");
-        model.put("name2", "삼동윤");
-        model.put("name3", "사동윤");
-        return new ModelAndView("hello", model);
+    public String helloPage(Model model) {
+        HelloModel helloModel = HelloModel.builder()
+                .name1("이동윤")
+                .name2("삼동윤")
+                .name3("사동윤")
+                .build();
+        model.addAttribute("helloModel", helloModel);
+        return "hello";
     }
 
     //그냥 String으로 리턴은 map으로 전달 불가능
