b5a90222c98b714a0b66a19dd734eb45a01914ab
dongyoon7212 <dongyoon7212@naver.com>
2024-03-08 12:13:30 +0900

    feat: DBStudy mysql

--- src/main/java/com/study/mvc/controller/DBController.java
+++ src/main/java/com/study/mvc/controller/DBController.java
@@ -1,6 +1,8 @@
 package com.study.mvc.controller;
 
 import com.study.mvc.dto.DBStudyReqDto;
+import com.study.mvc.service.DBStudyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DBController {
 
+    @Autowired
+    private DBStudyService dbStudyService;
+
     @PostMapping("/insert")
     public ResponseEntity<?> insert(@RequestBody DBStudyReqDto dbStudyReqDto) {
-        System.out.println(dbStudyReqDto);
-        return ResponseEntity.ok(null);
+
+        return ResponseEntity.ok(dbStudyService.createStudy(dbStudyReqDto));
     }
 }
