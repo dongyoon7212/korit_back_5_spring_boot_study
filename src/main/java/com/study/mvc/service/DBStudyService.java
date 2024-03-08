b135593f0fbd04f7a41138c6d58dc0beb4d04a2f
dongyoon7212 <dongyoon7212@naver.com>
2024-03-08 12:46:00 +0900

    feat: DBStudy mysql insert

--- src/main/java/com/study/mvc/service/DBStudyService.java
+++ src/main/java/com/study/mvc/service/DBStudyService.java
@@ -1,6 +1,7 @@
 package com.study.mvc.service;
 
 import com.study.mvc.dto.DBStudyReqDto;
+import com.study.mvc.dto.DBStudyRespDto;
 import com.study.mvc.entity.Study;
 import com.study.mvc.repository.DBStudyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ public class DBStudyService {
     @Autowired //DBStudyRepository는 컴포넌트가 아니다, 하지만 실질적으로 study.xml이 컴포넌트가 되기떄문에 Autowired가 가능하다.
     private DBStudyRepository dbStudyRepository;
 
-    public int createStudy(DBStudyReqDto dbStudyReqDto) {
+    public DBStudyRespDto createStudy(DBStudyReqDto dbStudyReqDto) {
 
         // 첫번째 방법 [ AllArgs ]
 //        Study study = new Study(0, dbStudyReqDto.getName(), dbStudyReqDto.getAge(), null);
@@ -27,6 +28,15 @@ public class DBStudyService {
                 .age(dbStudyReqDto.getAge())
                 .build();
 
-        return dbStudyRepository.save(study);
+        int successCount = dbStudyRepository.save(study);
+
+        DBStudyRespDto dbStudyRespDto = DBStudyRespDto.builder()
+                .id(study.getId()) // db에 저장 후 생성된 키값이 study객체에 들어가있음
+                .name(study.getName())
+                .age(study.getAge())
+                .successStatus(successCount > 0)
+                .successCount(successCount)
+                .build();
+        return dbStudyRespDto;
     }
 }
